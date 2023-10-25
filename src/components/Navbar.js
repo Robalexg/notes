f98c06ac29869c51ed7ed38a18205a5cf901d11c
robalexg <alexg7070@yahoo.com>
2023-10-25 13:57:14 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import '../styles/navbar.css'
+import logo from '../images/Minimalist Typography Simple Modern Brand Logo.png'; 
+import pic  from '../images/brooke-cagle-Nm70URdtf3c-unsplash.jpg'
 
 let Navbar = () => {
     return(
-        <div>
-            <img src="" alt="" />
-            <input type="text" />
-            <div>
-                <p>Robert</p>
-                <img src="" alt="" />
-                <img src="" alt="" />
-            </div>
+        <div className='navbar'>
+            <img id='navbar--logo' src={logo} alt="" />
+            <input id='navbar--search' type="text" />
+            <p id='navbar--name'>Robert</p>
+            <img id='navbar--pic' src={pic} alt="" />
+            
         </div>
     )
 }
+
 
 export default Navbar
